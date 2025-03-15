6ab320f955d897ec6652530134934281d6453262
tanua <@brdgt0128>
2025-03-15 14:50:57 +0800

    IMAGE

--- VAWCSanPedroHestia/Form2.Designer.cs
+++ VAWCSanPedroHestia/Form2.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.File_btn = new System.Windows.Forms.Button();
+            this.home_btn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.home_btn);
+            this.panel1.Controls.Add(this.File_btn);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(203, 852);
+            this.panel1.TabIndex = 0;
+            // 
+            // File_btn
+            // 
+            this.File_btn.Location = new System.Drawing.Point(50, 131);
+            this.File_btn.Name = "File_btn";
+            this.File_btn.Size = new System.Drawing.Size(114, 39);
+            this.File_btn.TabIndex = 1;
+            this.File_btn.Text = "FILE A CASE";
+            this.File_btn.UseVisualStyleBackColor = true;
+            // 
+            // home_btn
+            // 
+            this.home_btn.Location = new System.Drawing.Point(50, 190);
+            this.home_btn.Name = "home_btn";
+            this.home_btn.Size = new System.Drawing.Size(114, 39);
+            this.home_btn.TabIndex = 2;
+            this.home_btn.Text = "HOME";
+            this.home_btn.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(64, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(906, 852);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button home_btn;
+        private System.Windows.Forms.Button File_btn;
     }
 }
