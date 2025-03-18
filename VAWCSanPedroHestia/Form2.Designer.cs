3fc3ef8445132feadfb0ed288daec09630506c97
HajinCity <128238629+HajinCity@users.noreply.github.com>
2025-03-18 19:19:23 +0800

    Carlos Commit DATABASE

--- VAWCSanPedroHestia/Form2.Designer.cs
+++ VAWCSanPedroHestia/Form2.Designer.cs
@@ -66,9 +66,9 @@
             this.panel1.Controls.Add(this.File_btn);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(355, 884);
+            this.panel1.Size = new System.Drawing.Size(266, 718);
             this.panel1.TabIndex = 0;
             // 
             // logout_btn
@@ -80,21 +80,22 @@
             this.logout_btn.ForeColor = System.Drawing.Color.White;
             this.logout_btn.Image = ((System.Drawing.Image)(resources.GetObject("logout_btn.Image")));
             this.logout_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.logout_btn.Location = new System.Drawing.Point(60, 811);
-            this.logout_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.logout_btn.Location = new System.Drawing.Point(45, 659);
+            this.logout_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.logout_btn.Name = "logout_btn";
-            this.logout_btn.Size = new System.Drawing.Size(235, 47);
+            this.logout_btn.Size = new System.Drawing.Size(176, 38);
             this.logout_btn.TabIndex = 10;
             this.logout_btn.Text = "      LOGOUT";
             this.logout_btn.UseVisualStyleBackColor = true;
+            this.logout_btn.Click += new System.EventHandler(this.logout_btn_Click);
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(28, 181);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(21, 147);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(57, 50);
+            this.pictureBox1.Size = new System.Drawing.Size(43, 41);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -104,9 +105,10 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 9F);
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(108, 215);
+            this.label2.Location = new System.Drawing.Point(81, 175);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(187, 17);
+            this.label2.Size = new System.Drawing.Size(151, 15);
             this.label2.TabIndex = 9;
             this.label2.Text = "SYSTEM ADMINISTRATOR";
             // 
@@ -115,9 +117,10 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(108, 185);
+            this.label1.Location = new System.Drawing.Point(81, 150);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(192, 24);
+            this.label1.Size = new System.Drawing.Size(157, 19);
             this.label1.TabIndex = 1;
             this.label1.Text = "ADELFA ANTIPOLO";
             // 
@@ -129,13 +132,14 @@
             this.management_btn.ForeColor = System.Drawing.Color.White;
             this.management_btn.Image = ((System.Drawing.Image)(resources.GetObject("management_btn.Image")));
             this.management_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.management_btn.Location = new System.Drawing.Point(60, 667);
-            this.management_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.management_btn.Location = new System.Drawing.Point(45, 542);
+            this.management_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.management_btn.Name = "management_btn";
-            this.management_btn.Size = new System.Drawing.Size(235, 53);
+            this.management_btn.Size = new System.Drawing.Size(176, 43);
             this.management_btn.TabIndex = 8;
             this.management_btn.Text = "         SYSTEM \r\n       MANAGEMENT";
             this.management_btn.UseVisualStyleBackColor = true;
+            this.management_btn.Click += new System.EventHandler(this.management_btn_Click);
             // 
             // report_btn
             // 
@@ -145,13 +149,14 @@
             this.report_btn.ForeColor = System.Drawing.Color.White;
             this.report_btn.Image = ((System.Drawing.Image)(resources.GetObject("report_btn.Image")));
             this.report_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.report_btn.Location = new System.Drawing.Point(60, 601);
-            this.report_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.report_btn.Location = new System.Drawing.Point(45, 488);
+            this.report_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.report_btn.Name = "report_btn";
-            this.report_btn.Size = new System.Drawing.Size(235, 39);
+            this.report_btn.Size = new System.Drawing.Size(176, 32);
             this.report_btn.TabIndex = 7;
             this.report_btn.Text = "         REPORTS DOCS";
             this.report_btn.UseVisualStyleBackColor = true;
+            this.report_btn.Click += new System.EventHandler(this.report_btn_Click);
             // 
             // intake_btn
             // 
@@ -161,21 +166,21 @@
             this.intake_btn.ForeColor = System.Drawing.Color.White;
             this.intake_btn.Image = ((System.Drawing.Image)(resources.GetObject("intake_btn.Image")));
             this.intake_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.intake_btn.Location = new System.Drawing.Point(60, 534);
-            this.intake_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.intake_btn.Location = new System.Drawing.Point(45, 434);
+            this.intake_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.intake_btn.Name = "intake_btn";
-            this.intake_btn.Size = new System.Drawing.Size(240, 39);
+            this.intake_btn.Size = new System.Drawing.Size(180, 32);
             this.intake_btn.TabIndex = 6;
             this.intake_btn.Text = "       CALL SYSTEM";
             this.intake_btn.UseVisualStyleBackColor = true;
+            this.intake_btn.Click += new System.EventHandler(this.intake_btn_Click);
             // 
             // pictureBox2
             // 
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(120, 32);
-            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Location = new System.Drawing.Point(90, 26);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(113, 114);
+            this.pictureBox2.Size = new System.Drawing.Size(85, 93);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
@@ -188,13 +193,14 @@
             this.managecase_btn.ForeColor = System.Drawing.Color.White;
             this.managecase_btn.Image = ((System.Drawing.Image)(resources.GetObject("managecase_btn.Image")));
             this.managecase_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.managecase_btn.Location = new System.Drawing.Point(60, 471);
-            this.managecase_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.managecase_btn.Location = new System.Drawing.Point(45, 383);
+            this.managecase_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.managecase_btn.Name = "managecase_btn";
-            this.managecase_btn.Size = new System.Drawing.Size(240, 39);
+            this.managecase_btn.Size = new System.Drawing.Size(180, 32);
             this.managecase_btn.TabIndex = 4;
             this.managecase_btn.Text = "         MANAGE CASE LIST";
             this.managecase_btn.UseVisualStyleBackColor = true;
+            this.managecase_btn.Click += new System.EventHandler(this.managecase_btn_Click);
             // 
             // case_btn
             // 
@@ -204,13 +210,14 @@
             this.case_btn.ForeColor = System.Drawing.Color.White;
             this.case_btn.Image = ((System.Drawing.Image)(resources.GetObject("case_btn.Image")));
             this.case_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.case_btn.Location = new System.Drawing.Point(60, 409);
-            this.case_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.case_btn.Location = new System.Drawing.Point(45, 332);
+            this.case_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.case_btn.Name = "case_btn";
-            this.case_btn.Size = new System.Drawing.Size(240, 39);
+            this.case_btn.Size = new System.Drawing.Size(180, 32);
             this.case_btn.TabIndex = 3;
             this.case_btn.Text = "       CASE LIST";
             this.case_btn.UseVisualStyleBackColor = true;
+            this.case_btn.Click += new System.EventHandler(this.case_btn_Click);
             // 
             // home_btn
             // 
@@ -220,13 +227,14 @@
             this.home_btn.ForeColor = System.Drawing.Color.White;
             this.home_btn.Image = ((System.Drawing.Image)(resources.GetObject("home_btn.Image")));
             this.home_btn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.home_btn.Location = new System.Drawing.Point(60, 346);
-            this.home_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.home_btn.Location = new System.Drawing.Point(45, 281);
+            this.home_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.home_btn.Name = "home_btn";
-            this.home_btn.Size = new System.Drawing.Size(240, 39);
+            this.home_btn.Size = new System.Drawing.Size(180, 32);
             this.home_btn.TabIndex = 2;
             this.home_btn.Text = "        HOME";
             this.home_btn.UseVisualStyleBackColor = true;
+            this.home_btn.Click += new System.EventHandler(this.home_btn_Click);
             // 
             // File_btn
             // 
@@ -235,10 +243,10 @@
             this.File_btn.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold);
             this.File_btn.ForeColor = System.Drawing.Color.Transparent;
             this.File_btn.Image = global::VAWCSanPedroHestia.Properties.Resources.rect9;
-            this.File_btn.Location = new System.Drawing.Point(60, 257);
-            this.File_btn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.File_btn.Location = new System.Drawing.Point(45, 209);
+            this.File_btn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.File_btn.Name = "File_btn";
-            this.File_btn.Size = new System.Drawing.Size(245, 55);
+            this.File_btn.Size = new System.Drawing.Size(184, 45);
             this.File_btn.TabIndex = 1;
             this.File_btn.Text = "FILE A CASE";
             this.File_btn.UseVisualStyleBackColor = false;
@@ -248,32 +256,30 @@
             // 
             this.panel2.BackColor = System.Drawing.Color.White;
             this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel2.Location = new System.Drawing.Point(355, 0);
-            this.panel2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel2.Location = new System.Drawing.Point(266, 0);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(812, 38);
+            this.panel2.Size = new System.Drawing.Size(609, 31);
             this.panel2.TabIndex = 1;
             // 
             // panel3
             // 
             this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(355, 38);
-            this.panel3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panel3.Location = new System.Drawing.Point(266, 31);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(812, 846);
+            this.panel3.Size = new System.Drawing.Size(609, 687);
             this.panel3.TabIndex = 2;
             // 
             // Form2
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1167, 884);
+            this.ClientSize = new System.Drawing.Size(875, 718);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form2";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form2";
