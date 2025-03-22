d961f5ce35f7ba2fdc0cc5e2efe208085df4d0dc
tanua <@brdgt0128>
2025-03-22 20:43:22 +0800

    SystemManage

--- VAWCSanPedroHestia/Properties/Resources.Designer.cs
+++ VAWCSanPedroHestia/Properties/Resources.Designer.cs
@@ -149,5 +149,15 @@ namespace VAWCSanPedroHestia.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap security_official {
+            get {
+                object obj = ResourceManager.GetObject("security-official", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
